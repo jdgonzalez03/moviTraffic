18900a7ec69d173cfd9708d6ee0649897caf737d
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2024-10-14 17:43:47 -0500

    Initialize project

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+app.mount('#app')
